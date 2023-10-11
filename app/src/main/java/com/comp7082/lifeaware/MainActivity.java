353244c0bae792738c9e0e78d9268b906e4d825e
Shey Barpagga <sheybarpagga@gmail.com>
2023-10-11 16:23:11 -0700

    added navbar, profile and main page

--- app/src/main/java/com/comp7082/lifeaware/MainActivity.java
+++ app/src/main/java/com/comp7082/lifeaware/MainActivity.java
@@ -1,14 +1,42 @@
 package com.comp7082.lifeaware;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.comp7082.lifeaware.databinding.ActivityMainBinding;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new MainFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()) {
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.home:
+                    replaceFragment(new MainFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
