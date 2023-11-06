3dc62d103d98c95e5de0d7086525bf64a27fe284
SAMi-Rn <sami_roudgarian@yahoo.com>
2023-11-05 18:15:30 -0800

    assistant button functioning

--- app/src/main/java/com/comp7082/lifeaware/MainFragment.java
+++ app/src/main/java/com/comp7082/lifeaware/MainFragment.java
@@ -1,10 +1,12 @@
 package com.comp7082.lifeaware;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,24 +22,13 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 
 
-import android.Manifest;
-import android.app.AlertDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
 import android.os.Build;
-import android.os.Bundle;
-import android.telephony.SmsManager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
-import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
-import androidx.fragment.app.Fragment;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link MainFragment#newInstance} factory method to
@@ -88,60 +79,48 @@ public class MainFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view;
-        view = inflater.inflate(R.layout.fragment_main, container, false);
-        Button logoutButton = (Button) view.findViewById(R.id.LogoutButton);
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth mAuth = FirebaseAuth.getInstance();
-                mAuth.signOut();
-            }
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button logoutButton = view.findViewById(R.id.LogoutButton);
+        logoutButton.setOnClickListener(v -> {
+            FirebaseAuth mAuth = FirebaseAuth.getInstance();
+            mAuth.signOut();
         });
 
-        // Inflate the layout for this fragment
+        ImageView assistanceButton = view.findViewById(R.id.imageView3);
+        assistanceButton.setOnClickListener(v -> confirmAssistanceRequest());
+
         return view;
     }
-    /**
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        createNotificationChannel();
-
-        ImageView assistanceButton = rootView.findViewById(R.id.imageView3);
-        assistanceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                confirmAndSendSMS();
-            }
-        });
-
-        return rootView;
-    }
-
-    private void confirmAndSendSMS() {
+    private void confirmAssistanceRequest() {
+        sendNotificationWithDelay();
         new AlertDialog.Builder(getContext())
-                .setTitle("Send SMS")
-                .setMessage("Do you want to send an SMS for assistance?")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        sendNotification();
-                        sendTextMessage("The patient's number", "Assistance required!"); // replace with actual phone number
-                    }
+                .setTitle(getString(R.string.confirm_request))
+                .setMessage(getString(R.string.confirm_request_detail))
+                .setPositiveButton(getString(R.string.yes), (dialog, which) -> {
+                    // User confirmed to send the notification
+                    sendNotification();
                 })
-                .setNegativeButton("No", null)
-                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setNegativeButton(getString(R.string.no), (dialog, which) -> {
+                    // User cancelled the request
+                    Toast.makeText(getContext(), getString(R.string.assistance_cancelled), Toast.LENGTH_SHORT).show();
+                })
                 .show();
     }
 
+    private void sendNotificationWithDelay() {
+        // Set a delay before sending the notification (if necessary)
+        new Handler().postDelayed(this::sendNotification, 5000); // 5 second delay
+    }
+
+
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name); // Define this in strings.xml
-            String description = getString(R.string.channel_description); // Define this in strings.xml
+            CharSequence name = getString(R.string.PatientName); // Define this in strings.xml
+            String description = getString(R.string.Help); // Define this in strings.xml
             int importance = NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
@@ -151,32 +130,21 @@ public class MainFragment extends Fragment {
     }
 
     private void sendNotification() {
+        // Show a success message before sending the notification
+        Toast.makeText(getContext(), getString(R.string.notification_sent), Toast.LENGTH_LONG).show();
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(), CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_assist_icon) // replace with your actual icon
-                .setContentTitle("Assistance Request")
-                .setContentText("Jane Smith requires assistance.")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setSmallIcon(R.drawable.ic_baseline_health_and_safety_24) // add icon later
+                .setContentTitle(getString(R.string.notification_title))
+                .setContentText(getString(R.string.notification_message))
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
         NotificationManager notificationManager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1, builder.build());
-    }
 
-    private void sendTextMessage(String phoneNumber, String message) {
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.SEND_SMS}, PERMISSION_REQUEST_CODE);
-        } else {
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNumber, null, message, null, null);
-            Toast.makeText(getActivity(), "SMS sent.", Toast.LENGTH_LONG).show();
-        }
+        // Ensure the toast has time to display before the notification is sent and the activity state changes.
+        new Handler().postDelayed(() -> {
+            notificationManager.notify(1, builder.build());
+        }, Toast.LENGTH_LONG);
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            sendTextMessage("The patient's number", "Assistance required!"); // replace with the actual phone number
-        } else {
-            Toast.makeText(getActivity(), "Permission denied to send SMS.", Toast.LENGTH_SHORT).show();
-        }
-    }**/
 }
+
