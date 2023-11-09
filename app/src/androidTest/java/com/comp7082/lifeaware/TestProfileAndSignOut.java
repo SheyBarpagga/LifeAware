136e005ef5b66a18e1e8d1ad81d4059a6c8bffb0
sajjin <sajjinnijjar@gmail.com>
2023-11-08 16:00:40 -0800

    TestSignUp.java working passing test

---
+++ app/src/androidTest/java/com/comp7082/lifeaware/TestProfileAndSignOut.java
@@ -0,0 +1,118 @@
+package com.comp7082.lifeaware;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+// WARNING: Espresso Test Recorder was paused during recording.
+// The generated test may be missing actions which might lead to unexpected behavior.
+public class TestProfileAndSignOut {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testProfileAndSignOut() {
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.profile), withContentDescription("Profile"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottomNavigationView),
+                                        0),
+                                1),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.addPatient), withText("Add Patient"),
+                        childAtPosition(
+                                allOf(withId(R.id.contraint_layout),
+                                        childAtPosition(
+                                                withId(R.id.frame_layout),
+                                                0)),
+                                8),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction bottomNavigationItemView2 = onView(
+                allOf(withId(R.id.home), withContentDescription("Home"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottomNavigationView),
+                                        0),
+                                0),
+                        isDisplayed()));
+        bottomNavigationItemView2.perform(click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.LogoutButton), withText("Log Out"),
+                        childAtPosition(
+                                allOf(withId(R.id.contraint_layout),
+                                        childAtPosition(
+                                                withId(R.id.frame_layout),
+                                                0)),
+                                6),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.SignUpText), withText("Sign Up"),
+                        withParent(withParent(withId(R.id.frame_layout))),
+                        isDisplayed()));
+        textView.check(matches(withText("Sign Up")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
