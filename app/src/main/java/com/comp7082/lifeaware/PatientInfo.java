725404ae1c5e444b0452c984f58384bf3a75a075
Shey Barpagga <sheybarpagga@gmail.com>
2023-11-30 22:47:12 -0800

    added logic to display caregiver patient data

--- app/src/main/java/com/comp7082/lifeaware/PatientInfo.java
+++ app/src/main/java/com/comp7082/lifeaware/PatientInfo.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +16,6 @@ import android.view.ViewGroup;
  */
 public class PatientInfo extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public PatientInfo() {
         // Required empty public constructor
@@ -38,27 +31,31 @@ public class PatientInfo extends Fragment {
      */
     // TODO: Rename and change types and number of parameters
     public static PatientInfo newInstance(String param1, String param2) {
-        PatientInfo fragment = new PatientInfo();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        return new PatientInfo();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_patient_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_patient_info, container, false);
+
+        TextView name = view.findViewById(R.id.patient_info_name);
+        TextView age = view.findViewById(R.id.patient_info_age);
+        TextView notes = view.findViewById(R.id.patient_info_notes);
+
+        Bundle bundle = this.getArguments();
+
+        name.setText(bundle.getString("name"));
+        age.setText(bundle.getString("age"));
+        notes.setText((CharSequence) "No notes yet!");
+
+
+        return view;
     }
 }
